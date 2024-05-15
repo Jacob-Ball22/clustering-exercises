6b2cf85a26455b107e850a90dca0a6118a1a9f06
Jacob-Ball22 <jeball22@gmail.com>
2024-05-15 14:44:39 -0400

    Added acquire.py file for importing

---
+++ acquire.py
@@ -0,0 +1,12 @@
+#Imports
+import pandas as pd
+import os
+
+#Function that gets the mall_customers data from the csv file and returns it as a dataframe
+def get_mall_data():
+    filename = 'Mall_Customers.csv'
+    if os.path.exists(filename):
+        print('this file exists, reading from csv')
+        #read from csv
+        df = pd.read_csv(filename, index_col=0)
+        return df
